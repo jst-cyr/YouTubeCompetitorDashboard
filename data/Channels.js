10b1de65af679f5773bf2414f51de73b6e55f2f5
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2021-07-26 16:26:36 -0400

    Invoking YouTube API with multiple Channel IDs at once
    This next stage takes the existing implementation and optimizes the API calls to invoke only once for a list of channel IDs.
    
    Additionally, this moves the channel configuration to a separate JS file and retrieves additional statistics data from the API.

---
+++ data/Channels.js
@@ -0,0 +1,38 @@
+//This const represents the configuration of the list of the channels that will be displayed in the dashboard
+const channels = {
+	DiscoverSitecore: {
+		ChannelId : 'UCJrNXcAEmZrqbf2suxbfIkg',
+		Label : 'Discover Sitecore',
+		SubscriberCount : 0,
+		ViewCount : 0,
+		VideoCount : 0,
+	},
+	Sitecore: {
+		ChannelId : 'UCr6untnQn62iR3DkcswdKkw',
+		Label : 'Sitecore',
+		SubscriberCount : 0,
+		ViewCount : 0,
+		VideoCount : 0,
+	},
+	AdobeExperienceCloud: {
+		ChannelId: 'UCN-7ZEctit8Qu01BWeHQ0Fw',
+		Label : 'Adobe Experience Cloud',
+		SubscriberCount : 0,
+		ViewCount : 0,
+		VideoCount : 0,
+	},
+	Contentful: {
+		ChannelId: 'UCrsEcxPCJlz5yZ_FKGo6D9g',
+		Label : 'Contentful',
+		SubscriberCount : 0,
+		ViewCount : 0,
+		VideoCount : 0,
+	},
+	Optimizely: {
+		ChannelId: 'UCoLZaNd2rZ0mFPBD2aSPD7A',
+		Label : 'Optimizely (Episerver)',
+		SubscriberCount : 0,
+		ViewCount : 0,
+		VideoCount : 0,
+	}
+}
