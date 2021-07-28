61d27c9b540d5152894b1a3a4793e0404304e44b
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2021-07-28 10:18:03 -0400

    Converted object of channels into array of channels
    Simplified working with the channel data by converting the object of channels into an array of channel data.

--- data/Channels.js
+++ data/Channels.js
@@ -1,38 +1,38 @@
-//This const represents the configuration of the list of the channels that will be displayed in the dashboard
-const channels = {
-	DiscoverSitecore: {
+//This array of channel data is used as a datasource in the dashboard.
+const channels = [
+	{
 		ChannelId : 'UCJrNXcAEmZrqbf2suxbfIkg',
 		Label : 'Discover Sitecore',
 		SubscriberCount : 0,
 		ViewCount : 0,
 		VideoCount : 0,
 	},
-	Sitecore: {
+	{
 		ChannelId : 'UCr6untnQn62iR3DkcswdKkw',
 		Label : 'Sitecore',
 		SubscriberCount : 0,
 		ViewCount : 0,
 		VideoCount : 0,
 	},
-	AdobeExperienceCloud: {
+	{
 		ChannelId: 'UCN-7ZEctit8Qu01BWeHQ0Fw',
 		Label : 'Adobe Experience Cloud',
 		SubscriberCount : 0,
 		ViewCount : 0,
 		VideoCount : 0,
 	},
-	Contentful: {
+	{
 		ChannelId: 'UCrsEcxPCJlz5yZ_FKGo6D9g',
 		Label : 'Contentful',
 		SubscriberCount : 0,
 		ViewCount : 0,
 		VideoCount : 0,
 	},
-	Optimizely: {
+	{
 		ChannelId: 'UCoLZaNd2rZ0mFPBD2aSPD7A',
 		Label : 'Optimizely (Episerver)',
 		SubscriberCount : 0,
 		ViewCount : 0,
 		VideoCount : 0,
 	}
-}
+]
