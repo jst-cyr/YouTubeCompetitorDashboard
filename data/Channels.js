bb0e55b3ef08eb15fe8a02a92c42af699bd63a9e
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2021-07-28 10:27:16 -0400

    Removed statistics from channels array
    In order to simplify the channels object, the statistics have been removed from the configuration. This file is essentially going to represent the "CMS" list of channels we want to display on the table.  Actual data will come from the API.

--- data/Channels.js
+++ data/Channels.js
@@ -3,36 +3,21 @@ const channels = [
 	{
 		ChannelId : 'UCJrNXcAEmZrqbf2suxbfIkg',
 		Label : 'Discover Sitecore',
-		SubscriberCount : 0,
-		ViewCount : 0,
-		VideoCount : 0,
 	},
 	{
 		ChannelId : 'UCr6untnQn62iR3DkcswdKkw',
 		Label : 'Sitecore',
-		SubscriberCount : 0,
-		ViewCount : 0,
-		VideoCount : 0,
 	},
 	{
 		ChannelId: 'UCN-7ZEctit8Qu01BWeHQ0Fw',
 		Label : 'Adobe Experience Cloud',
-		SubscriberCount : 0,
-		ViewCount : 0,
-		VideoCount : 0,
 	},
 	{
 		ChannelId: 'UCrsEcxPCJlz5yZ_FKGo6D9g',
 		Label : 'Contentful',
-		SubscriberCount : 0,
-		ViewCount : 0,
-		VideoCount : 0,
 	},
 	{
 		ChannelId: 'UCoLZaNd2rZ0mFPBD2aSPD7A',
 		Label : 'Optimizely (Episerver)',
-		SubscriberCount : 0,
-		ViewCount : 0,
-		VideoCount : 0,
 	}
 ]
