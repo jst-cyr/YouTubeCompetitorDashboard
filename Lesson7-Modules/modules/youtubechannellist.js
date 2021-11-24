3b3100e802d838db71403b052ce82abf16d93cfa
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2021-11-24 06:43:00 -0500

    Initial Lesson 7 file checkin

---
+++ Lesson7-Modules/modules/youtubechannellist.js
@@ -0,0 +1,30 @@
+import { YouTubeData } from './youtubedata.js';
+
+class YouTubeChannelList {
+	constructor(apiKey, tableId, configuredChannels){
+		this.apiKey = apiKey;
+		this.tableId = tableId;
+		this.configuredChannels = configuredChannels;
+	}
+
+	//Update the markup with the latest data from the API
+	async fillTable(){
+		//Get a list of channel data from the API
+		var youtubeData = new YouTubeData(this.apiKey);
+		var channelsList = await youtubeData.getChannelData(this.configuredChannels);
+
+		//Sort the list of channels by their subscriber count, in descending order (biggest to smallest)
+		var sortedChannels = channelsList.sort((a,b) => b.SubscriberCount - a.SubscriberCount);
+
+		//For each of the channels in the sorted list, create a row and add it to the table
+		for(var index in sortedChannels){
+			//Create the row, showing a label and the subscriber count
+			var row = $("<tr><td>" + sortedChannels[index].Label + "</td><td>" + sortedChannels[index].SubscriberCount + "</td></tr>");
+
+			//Add the row to the table that has ID 'channelList'
+			$(this.tableId).find("tbody").append(row);
+		}
+	}
+}
+
+export {YouTubeChannelList};
---
+++ Lesson7-Modules/modules/youtubedata.js
@@ -0,0 +1,59 @@
+export {YouTubeData};
+
+class YouTubeData {
+	constructor(apiKey){
+		this.apiKey = apiKey;
+	}
+
+	//Get the subscriber count for a specified channel
+	async getChannelSubscribers(channelId) {
+		var subscriberCount = 
+			fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${this.apiKey}`)
+			.then(response => {
+				return response.json()
+			})
+			.then(data => {
+				console.log(data);
+				return data["items"][0].statistics.subscriberCount;
+			})
+			return subscriberCount;
+		}
+
+	//Invokes the YouTube API to get data about all channels provided in the list of channels
+	//RETURNS: An array of channel objects with the YouTube statistics data
+	async getChannelData(channelsList){
+		//Build up a list of the IDs we want to get data for, separated by a comma
+		var channelIds = channelsList.map((channel) => channel.ChannelId).join();
+		
+		//Fetch the data and then fill out the objects
+		var youtubeChannelData = 
+			fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelIds}&key=${this.apiKey}`)
+			.then(response => {
+				return response.json()
+			})
+			.then(data => {
+				console.log(data);
+
+				//Map the data from YouTube into a new array of channel objects
+				var channelData = data["items"].map(item => 
+				{
+					//Find the matching channel in the list that was provided
+					var matchedChannel = channelsList.find(channel => channel.ChannelId === item.id);
+
+					//Create a new object combining existing config data and the data from the YouTube API
+					return {
+					ChannelId: item.id,
+					Label: matchedChannel.Label,
+					SubscriberCount: item.statistics.subscriberCount,
+					ViewCount: item.statistics.viewCount,
+					VideoCount: item.statistics.videoCount,
+					}
+				}
+				)
+
+				return channelData;
+			});
+
+		return youtubeChannelData;
+	}
+}
