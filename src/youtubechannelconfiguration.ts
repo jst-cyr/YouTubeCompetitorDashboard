2ed7390824f76affef94b938326d8544fdd408ed
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2021-12-21 17:18:18 -0500

    TypeScript conversion: Strongly typing ChannelConfig
    Added a new class that allows for us to strongly type the return of the method getChannelConfiguration()

--- src/youtubechannelconfiguration.ts
+++ src/youtubechannelconfiguration.ts
@@ -1,9 +1,21 @@
-export {YouTubeChannelConfiguration};
+export {YouTubeChannelConfiguration, ChannelConfig};
 
 //This hack allows TypeScript to skip validating the 'require' statement in getChannelConfiguration(). 
 //In future steps, we should run on Node and import Airtable as a proper module.
 declare var require: any;
 
+//Class used to store data about a specific channel received from a data source
+class ChannelConfig {
+	Label: string;
+	ChannelId: string;
+
+	constructor(label: string, channelId: string){
+		this.Label = label;
+		this.ChannelId = channelId;
+	}
+}
+
+//Class used to retrieve all channel configuration from a data source
 class YouTubeChannelConfiguration {
 	apiKey: string;
 	databaseId: string;
@@ -14,7 +26,7 @@ class YouTubeChannelConfiguration {
 	}
 
 	//Retrieves the array of channels configured for the dashboard that we need to get data for from the API
-	async getChannelConfiguration(){
+	async getChannelConfiguration(): Promise<ChannelConfig[]>{
 		//Load the Airtable API and specify authentication and the base we want to connect to
 		var Airtable = require('airtable');
 		Airtable.configure({
@@ -27,12 +39,12 @@ class YouTubeChannelConfiguration {
 		var records = await base('Channels').select().all();
 
 		//Create the array of configured channels
-		var channelConfigList = new Array();
+		var channelConfigList = new Array<ChannelConfig>();
 		records.forEach(function(record: any){
-			var channelConfig = {
-				Label: record.get('Label'),
-				ChannelId: record.get('ChannelId')
-			}
+			var channelConfig = new ChannelConfig(
+				record.get('Label'), 
+				record.get('ChannelId')
+			)
 			//Add to the list of channel configuration
 			channelConfigList.push(channelConfig);
 		})
