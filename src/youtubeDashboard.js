8db3a9a2e9efe73bab7a04fb34de3da187ea1c14
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2021-12-22 11:34:34 -0500

    Switching to 'const' from 'var' in Dashboard js

--- src/youtubeDashboard.js
+++ src/youtubeDashboard.js
@@ -2,20 +2,20 @@ import { YouTubeChannelConfiguration } from '../dist/youtubechannelconfiguration
 import { YouTubeChannelList } from '../src/youtubechannellist.js';
           
 //API Key used for accessing the YouTube API
-var youtubeApiKey = 'AIzaSyClnRkQON9_oUA9nXShlGKOwLXGRG2sqvY';
+const youtubeApiKey = 'AIzaSyClnRkQON9_oUA9nXShlGKOwLXGRG2sqvY';
 
 //Airtable identifiers for the API
-var airtableBaseId = 'appDuKh62DrdrfGGy';
-var readonlyApiKey = 'keyfGNPlzxgkVj5Eo';
+const airtableBaseId = 'appDuKh62DrdrfGGy';
+const readonlyApiKey = 'keyfGNPlzxgkVj5Eo';
 
 //Update the markup with the latest data from the API
 async function updateStats(){
   //Get the configured list of channels we want to show on the dashboard
-  var youtubeChannelConfiguration = new YouTubeChannelConfiguration(readonlyApiKey, airtableBaseId);
-  var configuredChannels = await youtubeChannelConfiguration.getChannelConfiguration();
+  const youtubeChannelConfiguration = new YouTubeChannelConfiguration(readonlyApiKey, airtableBaseId);
+  const configuredChannels = await youtubeChannelConfiguration.getChannelConfiguration();
 
   //Fill out the table in the markup with a new YouTube Channel listing
-  var youtubeChannelList = new YouTubeChannelList(youtubeApiKey, "#channelList", configuredChannels);
+  const youtubeChannelList = new YouTubeChannelList(youtubeApiKey, "#channelList", configuredChannels);
   youtubeChannelList.fillTable();
 }
 
