d8a550695c64f23f62c8b93ca8ccf9120a2c77c8
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2022-09-09 16:03:15 -0400

    Updated Azure Function to use Google API and extract statistics
    Web 'fetch' was not working on the server side, even using 'next-fetch'

--- CaptureSubscribers/index.js
+++ CaptureSubscribers/index.js
@@ -1,21 +1,31 @@
+const {google} = require('googleapis');
+
 module.exports = async function (context, req) {
+    
     context.log('Beginning capture of YouTube subscribers.');
 
+    //Setup the request data we need to call the API
     const apiKey = 'AIzaSyClnRkQON9_oUA9nXShlGKOwLXGRG2sqvY';
     const channelId = (req.query.name || (req.body && req.body.name));
-    const subscriberCount = 
-        fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`)
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            context.log(data);
-            return data["items"][0].statistics.subscriberCount;
-        });
-    
-    const responseMessage = channelId
-        ? "Captured " + subscriberCount + " subscribers for " + channelId + "."
-        : "This HTTP triggered function executed successfully. Pass a channel ID in the query string or in the request body to retrieve subscriber count.";
+
+    //Call the YouTube API for the channel data
+    const youtube = google.youtube({
+        version: 'v3',
+        auth: apiKey,
+    });
+
+    const youtubeResponse = await youtube.channels.list({
+        part: 'id,statistics',
+        id: channelId,
+    })
+
+    //Extract the data from the response, if possible
+    var responseMessage = "This HTTP triggered function executed successfully. Pass a channel ID in the query string or in the request body to retrieve subscriber count.";
+    if(youtubeResponse.data && youtubeResponse.data.items && youtubeResponse.data.items.length){
+        const { statistics: { subscriberCount, videoCount, viewCount } } = youtubeResponse.data.items[0];
+        responseMessage = `The channel ${channelId} has ${subscriberCount} subscribers, ${videoCount} videos and ${viewCount} views.`;
+        console.log(responseMessage);
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
