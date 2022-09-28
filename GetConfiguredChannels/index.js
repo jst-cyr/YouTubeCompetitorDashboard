b5c080c54646f12a08b8b8db265d53b32f2e24e8
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2022-09-28 15:14:37 -0400

    Updated new Airtable function to have code from the application

--- GetConfiguredChannels/index.js
+++ GetConfiguredChannels/index.js
@@ -1,13 +1,45 @@
+const airtable = require('airtable');
+
+//Class used to store data about a specific channel received from a data source
+class ChannelConfig {
+	Label;
+	ChannelId;
+
+	constructor(label, channelId){
+		this.Label = label;
+		this.ChannelId = channelId;
+	}
+}
+
 module.exports = async function (context, req) {
     context.log('Loading configured channels from data store.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    //Load the Airtable API and specify authentication and the base we want to connect to
+    const apiKey = 'keyfGNPlzxgkVj5Eo';
+    const databaseId = 'appDuKh62DrdrfGGy';
+    airtable.configure({
+        endpointUrl: 'https://api.airtable.com',
+        apiKey: apiKey
+    });
+    var base = airtable.base(databaseId);
 
+    //Select the list of records from the Airtable 
+    var records = await base('Channels').select().all();
+    context.log(`Airtable records: ${records}`)
+
+    //Create the array of configured channels
+    var channelConfigList = new Array();
+    records.forEach(function(record){
+        var channelConfig = new ChannelConfig(
+            record.get('Label'), 
+            record.get('ChannelId')
+        );
+        //Add to the list of channel configuration
+        channelConfigList.push(channelConfig);
+    })
+
+    context.log(`Configured channels: ${channelConfigList}`);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: channelConfigList
     };
 }
